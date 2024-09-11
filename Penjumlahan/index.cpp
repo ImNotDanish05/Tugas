a248161afb8a91084ff3a127be8a3ca1700118fb
ImNotDanish05 <danishmahdi2005@gmail.com>
2024-09-11 21:57:39 +0700

    first commit

---
+++ Penjumlahan/index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 0; // Satu-satunya variabel
+    for (int b = 1; b < 100; b++){
+        cout << a << " + " << b << " = ";
+        a = a + b;
+        cout << a << " Loop ke: " << b << endl;
+    }
+    cout << "Hasil: " << a;
+    return 0;
+}
---
+++ Suhu/PRNO2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double c, f, r;
+    cout << "Please put an number in Celcius: ";
+    cin >> c;
+    r = c * 4 / 5;
+    f = (c * 1.8) + 32;
+    cout << fixed << setprecision(2);
+    cout << "Celcius: " << c << endl;
+    cout << "Reamur: " << r << endl;
+    cout << "Fahrenheit: " << f << endl;
+    return 0;
+}
